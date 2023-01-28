feb64634e7abe4da944ff464029f5035e193ba99
jasmaa <jasonmaa3955@gmail.com>
2023-01-28 15:54:08 -0500

    Set headless chrome

--- src/test/java/com/github/canary/Utils.java
+++ src/test/java/com/github/canary/Utils.java
@@ -2,10 +2,15 @@ package com.github.canary;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Utils {
   public static WebDriver getDriver() {
-    WebDriver driver = new ChromeDriver();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-debugging-port=9222");
+    options.addArguments("--headless=new");
+
+    WebDriver driver = new ChromeDriver(options);
     return driver;
   }
 }
