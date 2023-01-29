0aff8c4a7c6344f3fc449d09b7a5d9f803adc60f
jasmaa <jasonmaa3955@gmail.com>
2023-01-29 12:26:34 -0500

    Remove cw publish from testng. Add other suites. Add fail and skip cases.

--- src/test/java/com/github/jasmaa/testngcanaryexample/DummyTest.java
+++ src/test/java/com/github/jasmaa/testngcanaryexample/DummyTest.java
@@ -1,11 +1,22 @@
 package com.github.jasmaa.testngcanaryexample;
 
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 public class DummyTest {
   @Test
   public void shouldAnswerWithTrue() {
     Assert.assertTrue(true);
+  }
+
+  @Test
+  public void shouldFail() {
+    Assert.fail("Fail this");
+  }
+
+  @Test
+  public void shouldSkip() {
+    throw new SkipException("Skip this");
   }
 }
